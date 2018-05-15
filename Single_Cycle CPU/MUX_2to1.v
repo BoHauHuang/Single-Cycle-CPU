245e75315095069788fa12ec0d4d495ad5768375
BoHau Huang <31922162+BoHauHuang@users.noreply.github.com>
2018-05-15 19:29:48 +0800

    Add files via upload

---
+++ Single_Cycle CPU/MUX_2to1.v
@@ -0,0 +1,23 @@
+module MUX_2to1(
+               data0_i,
+               data1_i,
+               select_i,
+               data_o
+               );
+
+parameter size = 0;
+			
+//I/O ports               
+input   [size-1:0] data0_i;          
+input   [size-1:0] data1_i;
+input              select_i;
+output  [size-1:0] data_o; 
+
+//Internal Signals
+wire     [size-1:0] data_o;
+//Main function
+assign data_o = (select_i)? data1_i:data0_i;
+
+endmodule      
+          
+          
